17b729b0d3209f99679b3e74c38e51e36dba313f
Anne Marie O'Sullivan <annemarieos.irl@gmail.com>
2019-08-10 15:04:00 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.random()
+
+const mod = 42 - 38
+
+const max = Math.max(1, 4, 2, 18, 20)
+
