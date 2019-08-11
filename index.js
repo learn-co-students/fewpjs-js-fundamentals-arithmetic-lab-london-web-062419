1eaba18f88343906c4179ef9632762af78428594
slumfrog <kishan.anil@gmail.com>
2019-08-11 15:14:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// multiply
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+//random number above 0
+const random = Math.floor( Math.random() * 1000 ) + 1
+
+// generates remainder that is equal to 4
+const mod = 16 % 6
+
+//Return 20 as highest number in the set
+const max = Math.max( 5, 10, 15, 20 )
